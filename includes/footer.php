6e5bbf595d55317cbdf4faa7414c611261bcfb26
kola192 <mutwakil96@gmail.com>
2020-11-17 07:43:59 +0200

    Styled the footer.

--- includes/footer.php
+++ includes/footer.php
@@ -2,21 +2,21 @@
 <footer id="footer">
     <div class="footer-top">
       <div class="container">
-        <div class="row">
+        <div class="row justify-content-between">
 
           <div class="col-lg-3 col-md-6 footer-info">
-            <h3>BizPage</h3>
+            <h3>Reyada</h3>
             <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet proin fermentum leo. Amet volutpat consequat mauris nunc congue.</p>
           </div>
 
           <div class="col-lg-3 col-md-6 footer-links">
             <h4>Useful Links</h4>
             <ul>
-              <li><i class="ion-ios-arrow-right"></i> <a href="#">Home</a></li>
-              <li><i class="ion-ios-arrow-right"></i> <a href="#">About us</a></li>
-              <li><i class="ion-ios-arrow-right"></i> <a href="#">Services</a></li>
-              <li><i class="ion-ios-arrow-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="ion-ios-arrow-right"></i> <a href="#">Privacy policy</a></li>
+              <li><i class="ion-ios-arrow-right"></i> <a href="index.php">Home</a></li>
+              <li><i class="ion-ios-arrow-right"></i> <a href="about.php">About us</a></li>
+              <li><i class="ion-ios-arrow-right"></i> <a href="index.php#services">Services</a></li>
+              <li><i class="ion-ios-arrow-right"></i> <a href="#">Projects</a></li>
+              <li><i class="ion-ios-arrow-right"></i> <a href="contact.php">Contact Us</a></li>
             </ul>
           </div>
 
@@ -40,13 +40,20 @@
 
           </div>
 
-          <div class="col-lg-3 col-md-6 footer-newsletter">
-            <h4>Our Newsletter</h4>
-            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna veniam enim veniam illum dolore legam minim quorum culpa amet magna export quem marada parida nodela caramase seza.</p>
-            <form action="" method="post">
-              <input type="email" name="email"><input type="submit" value="Subscribe">
-            </form>
-          </div>
+
+          <!-- can be uncommented -->
+
+          <!-- <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Branches</h4>
+            <ul>
+              <li><i class="ion-ios-arrow-right"></i> <a href="index.php">Red Se</a></li>
+              <li><i class="ion-ios-arrow-right"></i> <a href="index.php">White Nile</a></li>
+              <li><i class="ion-ios-arrow-right"></i> <a href="about.php">Blue Nile</a></li>
+              <li><i class="ion-ios-arrow-right"></i> <a href="index.php#services">North Darfour</a></li>
+              <li><i class="ion-ios-arrow-right"></i> <a href="#">West Darfour</a></li>
+              <li><i class="ion-ios-arrow-right"></i> <a href="contact.php">Khartoum</a></li>
+            </ul>
+          </div>             -->
 
         </div>
       </div>
@@ -54,16 +61,11 @@
 
     <div class="container">
       <div class="copyright">
-        &copy; Copyright <strong>BizPage</strong>. All Rights Reserved
+        &copy; Copyright <strong>Reyada</strong> 2020. All Rights Reserved
       </div>
       <div class="credits">
-        <!--
-        All the links in the footer should remain intact.
-        You can delete the links only if you purchased the pro version.
-        Licensing information: https://bootstrapmade.com/license/
-        Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=BizPage
-      -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        
+        Designed by <a href="#">Green Design</a>
       </div>
     </div>
   </footer><!-- End Footer -->
